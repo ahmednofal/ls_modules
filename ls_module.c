49a81c07da553dc9e7d150bab77d78c3966db96e
naufal <ahmednofal@aucegypt.edu>
2017-03-26 22:57:53 +0200

    The second Commit 	*This is a description of what has been achieved to the date of 	the commit
    	*The added changes are the Makefile changes to easen the entire compi-
    	lation and data storage steps
    
    	*Now the entire output is achieved by the command make and
    	then examining the output files, DFS_output.txt for the the output of the
    	DFS_traversal, and the ps_efl_output.txt for the command ps -eFl that
    	lists all of the running tasks

--- ls_module.c
+++ ls_module.c
@@ -3,31 +3,65 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/list.h>
+
 //#include <bits/sched.h>
 
 struct task_struct *task;
-// Traverse tasks as a dfs
-void tasks_traversal_as_DFS()
+#define WIDTHOFWHITESPACES -20
+/*
+    *priniting_mesg prints out the headlines of each of the three columns
+    *printing_tasks_info prints out the name, state and pid of each task
+    *   task_comm is the name of the task
+    *   task_state is the state of the task
+    *   task_pid is the process id of the task
+*/
+#define printing_mesg printk("%-20s%-20s%s", "TASKNAME", "STATE", "PID");
+#define printing_tasks_info(task_comm, task_state, task_pid) \
+    printk("\n%-20s%-20ld%-20d\n", task_comm, task_state, task_pid);
+
+/*
+    *tasks_traversak_as_DFS uses the macros list_for_each, list_entry to output the name, state and pid of each task
+    *in a Depth First Search manner utilizing the tree nature of the processes struct in linux
+    *   list_for_each is from the list.h file, description can be found there
+    *   list_entry is in the list.h description can be found there
+*/
+//printk("%-20%-20%s", "TASKNAME", "STATE", "PID");
+
+void tasks_traversal_as_DFS(struct task_struct *head_task)
 {
-    printk("\nPrinting tasks normally \n\n");
-    printk("%-20s%-20s%s", "TASKNAME", "STATE", "PID");
-    for_each_process(task)
+
+    struct list_head *list;
+    struct list_head *head_task_children_list = &(head_task->children);
+    //struct list_head *current_task_children_list;
+
+    // Children, Sibling, list_for_each, struct list head, init_task, list_entry
+    if (head_task!= NULL)
     {
-        printk("\n%-20s%-20ld%-20d\n", task->comm, task->state, task->pid);
+        printing_tasks_info(head_task->comm, head_task->state, head_task->pid);
+        list_for_each(list, head_task_children_list)
+        {
+            task = list_entry(list, struct task_struct, sibling);
+            /* task points to the next child in the list */
+            tasks_traversal_as_DFS(task);
+        }
     }
 }
-void tasks_traversal()
+
+void tasks_traversal(void)
 {
-    printk("%-20s%-20s%s", "TASKNAME", "STATE", "PID");
+    printing_mesg;
     for_each_process(task)
     {
-        printk("\n%-20s%-20ld%-20d\n", task->comm, task->state, task->pid);
+        printing_tasks_info(task->comm, task->state, task->pid);
     }
 }
 int module_start(void)
 {
-    tasks_traversal();
-    tasks_traversal_as_DFS();
+    //tasks_traversal();
+    printk("\nPrinting tasks DFS Mode \n\n");
+    printing_mesg;
+    tasks_traversal_as_DFS(&init_task);
 
     return 0;
 }
---
+++ macro.c
@@ -0,0 +1,7 @@
+#define printing_macro printf("hello world from macro");
+
+int main()
+{
+    printing_macro;
+    return 0;
+}
